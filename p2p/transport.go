73ba9a3747fdf2040b666fe6d5cf244dc691b621
Montek <87750128+Montekkundan@users.noreply.github.com>
2024-06-19 13:10:03 -0700

    inital commit

---
+++ p2p/transport.go
@@ -0,0 +1,8 @@
+package p2p
+
+// Peer represents the remote node
+type Peer interface{}
+
+// Transport handles the communication between nodes
+// in the network. It can be TCP, UDP, etc.
+type Transport interface{}
